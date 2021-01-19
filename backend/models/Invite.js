1286e773fccf8f2b2091ac56a028b3299529bb2f
Karolin Albinsson <karolin.albinsson@gmail.com>
2021-01-19 23:32:50 +0100

    changed in endpoints and models removing the invites

--- backend/models/Invite.js
+++ backend/models/Invite.js
@@ -1,22 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 //invite user to collab
 export const inviteSchema = new mongoose.Schema({
-  createdBy: {
+	createdBy: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+	},
+	//not registered user
+	createdForEmail: {
+		type: String,
+		required: true,
+	},
+	/* createdForUserId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-  },
-  //not registered user
-  createdForEmail: {
-    type: String,
-    required: true,
-  },
-  createdForUserId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  projectId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Project',
-  },
+  },*/
+	projectId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Project",
+	},
 });
