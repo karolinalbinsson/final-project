5338c8df46bfd1336ad0da478b1dadb686e8a38d
Karolin Albinsson <karolin.albinsson@gmail.com>
2021-02-11 13:54:23 +0100

    changed url in maillink

--- backend/email-templates/emailTemplates.js
+++ backend/email-templates/emailTemplates.js
@@ -1,9 +1,8 @@
 //For creating some basic html to send to the user.
 export const createHtmlInvite = (toUser, fromUser) => {
 	let htmlstring = `<h2>Hey!</h2><br>`;
-	htmlstring += `${fromUser} has invited you to collaborate on a project in project planner.<br>`;
-	htmlstring += `<a href="https://www.google.se/signUp">Sign in or sign up and get access to the project<a>`;
-	htmlstring += `<p> The link in plain text: https://www.google.se/signUp </p>`;
+	htmlstring += `${fromUser} has invited you to collaborate on a project in Project Planner.<br>`;
+	htmlstring += `<a href="https://project-planner-tsk.netlify.app/">Sign in or sign up and get access to the project!<a>`;
 	return htmlstring;
 };
 
