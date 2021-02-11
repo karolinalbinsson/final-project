b61899f425db7d3cdd73db7a25ddb0e61d2f75f3
Karolin Albinsson <karolin.albinsson@gmail.com>
2021-02-11 13:49:04 +0100

    removed unused vars

--- frontend/src/lib/BackdropLoader.js
+++ frontend/src/lib/BackdropLoader.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import Backdrop from '@material-ui/core/Backdrop';
-import { useSelector } from 'react-redux';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import React from "react";
+import Backdrop from "@material-ui/core/Backdrop";
+import { useSelector } from "react-redux";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
-  },
-  root: {
-    width: '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
-  },
+const useStyles = makeStyles((theme) => ({
+	backdrop: {
+		zIndex: theme.zIndex.drawer + 1,
+		color: "#fff",
+	},
+	root: {
+		width: "100%",
+		"& > * + *": {
+			marginTop: theme.spacing(2),
+		},
+	},
 }));
 
 const BackdropLoader = () => {
-  const classes = useStyles();
-  const isLoading = useSelector(store => store.ui.isLoading);
-  console.log('Backdrop loader loading:', isLoading);
+	const classes = useStyles();
+	const isLoading = useSelector((store) => store.ui.isLoading);
+	console.log("Backdrop loader loading:", isLoading);
 
-  return (
-    <Backdrop
-      transitionDuration={2000}
-      className={classes.backdrop}
-      open={isLoading}
-    >
-      <CircularProgress color="inherit" />
-    </Backdrop>
-  );
+	return (
+		<Backdrop
+			transitionDuration={2000}
+			className={classes.backdrop}
+			open={isLoading}
+		>
+			<CircularProgress color="inherit" />
+		</Backdrop>
+	);
 };
 export default BackdropLoader;
